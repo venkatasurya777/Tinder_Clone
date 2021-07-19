aed108ab8bbfb1088355ab4e0cf764cb11e393ba
Vudutha Venkata Surya Kiran <67712163+venkatasurya777@users.noreply.github.com>
2021-07-19 13:44:01 +0530

    Add files via upload

---
+++ Server/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cards from "./dbCards.js";
+import Cors from "cors";
+
+//App Config
+const app = express();
+const port = process.env.PORT || 7000;
+const connection_url =
+  "mongodb+srv://tinder:tinder123@cluster0.q4n21.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+//middlewares
+app.use(express.json());
+app.use(Cors());
+
+//DB Config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+//API Endpoints
+app.get("/", (req, res) => res.status(200).send("HELLO SURYA"));
+
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/tinder/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+//Listener
+app.listen(port, () => console.log(`Api is running at: ${port}`));
